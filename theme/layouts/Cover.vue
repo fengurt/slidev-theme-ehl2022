9680e081e54e6b16c2f81e450115dca98cbb080f
pkuasule <pkuasule@users.noreply.github.com>
2023-05-16 12:40:42 +0800

    adjust cover canvas

--- theme/layouts/Cover.vue
+++ theme/layouts/Cover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="slidev-layout Cover">
-    <div class="my-auto border-t border-stone-500 mt-3">
+  <div class="slidev-layout  h-full w-full Cover">
+    <div class="my-auto border-t border-stone-500 mt-3 h-full w-full">
       <slot />
       <quality-assurance class="absolute fill-stone-500 top-25 right-30" width="100" />
       <committed-to-service-excellence class="absolute fill-white top-10 right-10" width="120" />
